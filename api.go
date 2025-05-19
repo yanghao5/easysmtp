df838ae332ee8b1ce58d5b7c97be252cc04835d9
yanghao5 <yanghao.buffer@outlook.com>
2025-05-19 15:35:32 +0800

    [feat](easysmtp) add Send()

--- api.go
+++ api.go
@@ -1,51 +1,78 @@
 package easysmtp
 
-func EasySend(msg string) (err error) {
-	_ = msg
-	return nil
-}
-func SendMail(recipient string, cc []string, subject string, msg string) (err error) {
-	_ = recipient
-	_ = cc
-	_ = subject
-	_ = msg
+import (
+	"easysmtp/common/conf"
+	"easysmtp/common/validator"
+	"easysmtp/internal"
+	"fmt"
+	"strings"
 
-	return nil
-}
-func Gmail(recipient string, cc []string, subject string, msg string) (err error) {
-	_ = recipient
-	_ = cc
-	_ = subject
-	_ = msg
+	netsmtp "net/smtp"
+)
 
-	return nil
-}
-func Outlook(recipient string, cc []string, msg string, subject string) (err error) {
-	_ = recipient
-	_ = cc
-	_ = subject
-	_ = msg
+func Send(config conf.Config) (ret string, err error) {
+	// sender
+	sender := ""
+	if validator.IsValidEmail(config.Sender) {
+		sender = config.Sender
+	} else {
+		return "", fmt.Errorf("invalid sender email: %s", config.Sender)
+	}
 
-	return nil
-}
-func QQFoxmail(recipient string, cc []string, msg string, subject string) (err error) {
-	_ = recipient
-	_ = cc
-	_ = subject
-	_ = msg
+	// recipient
+	recipient := ""
+	if validator.IsValidEmail(config.Recipient) {
+		recipient = config.Recipient
+	} else {
+		return "", fmt.Errorf("invalid recipient email: %s", config.Recipient)
+	}
 
-	return nil
-}
+	// Cc
+	validCC := []string{}
+	for _, addr := range config.CC {
+		if validator.IsValidEmail(addr) {
+			validCC = append(validCC, addr)
+		}
+	}
 
-func Send(smtp_server string, sender string, name string, passwd string, recipient string, subject string, msg string, cc []string, enable_html bool) (ret string, err error) {
-	_ = smtp_server
-	_ = sender
-	_ = name
-	_ = passwd
-	_ = recipient
-	_ = subject
-	_ = msg
-	_ = cc
-	_ = enable_html
+	// 构造邮件头
+	header := map[string]string{
+		"From":    fmt.Sprintf("%s <%s>", config.Name, sender),
+		"To":      recipient,
+		"Subject": config.Subject,
+	}
+
+	if len(validCC) > 0 {
+		header["Cc"] = strings.Join(validCC, ", ")
+	}
+	if config.EnableHTML {
+		header["Content-Type"] = "text/html; charset=UTF-8"
+	} else {
+		header["Content-Type"] = "text/plain; charset=UTF-8"
+	}
+
+	var message strings.Builder
+	for k, v := range header {
+		message.WriteString(fmt.Sprintf("%s: %s\r\n", k, v))
+	}
+	message.WriteString("\r\n" + config.Msg)
+
+	allRecipients := append([]string{config.Recipient}, validCC...)
+
+	auth := netsmtp.PlainAuth("", config.Sender, config.Passwd, config.SmtpServer)
+
+	err = internal.SendMailUsingSTARTTLS(
+		fmt.Sprintf("%s:%d", config.SmtpServer, 587),
+		auth,
+		config.Sender,
+		allRecipients,
+		[]byte(message.String()),
+	)
+
+	if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("Send mail success!")
+	}
 	return "", nil
 }
