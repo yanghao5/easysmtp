0cb758b160005c5564ce3a805a0dba03d9d80375
yanghao5 <yanghao.buffer@outlook.com>
2025-05-19 16:58:18 +0800

    [feat](common/validator) add IsEmailFromProvider()

--- common/validator/validator.go
+++ common/validator/validator.go
@@ -1,8 +1,21 @@
 package validator
 
-import "net/mail"
+import (
+	"net/mail"
+	"strings"
+)
 
 func IsValidEmail(email string) bool {
 	_, err := mail.ParseAddress(email)
 	return err == nil
+}
+
+func IsEmailFromProvider(email string, provider string) bool {
+	at := strings.LastIndex(email, "@")
+	if at == -1 {
+		return false
+	}
+	domain := email[at+1:]
+
+	return strings.Contains(domain, provider)
 }
